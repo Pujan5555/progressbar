2bbe62298a9f44d70181017bb7bcef725277973f
Pujan <puzanchakma@gmail.com>
2024-07-02 20:38:05 +0600

    version 1.0 time 8.37pm date 02.07.2024

---
+++ script.js
@@ -0,0 +1,23 @@
+let click = document.querySelector('.btn');
+let reset = document.querySelector('.reset');
+let myInput = document.getElementById("input").value;
+let progress = document.querySelector('#progressDone');
+let score = myInput;
+let time = 3000 / score;
+click.addEventListener('click', (e) => {
+    let counter = 0;
+    progress.classList.add('progress-bar');
+    setInterval((e) => {
+        if(counter < score){
+            counter ++;
+            progress.style.width = `${counter}%`;
+            progress.innerText = `${counter}%`;
+        }
+    }, time);
+    progress.style.height = "100%";
+});
+reset.addEventListener('click', (e) => {
+    progress.classList.remove('progress-bar');
+    progress.style.width = `${0}%`;
+    progress.innerText = ``;
+});
